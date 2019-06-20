a2cd7521acf996d7821aaa12b3a8927baacd5c9a
dsrtf0x <47214918+dsrtf0x@users.noreply.github.com>
2019-06-20 21:52:40 +0300

    Add files via upload

---
+++ ECO/js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+$(document).ready(() => {
+
+	$('#loginButton').click(() => {
+		$('#loginSection').slideDown(1500);
+	});
+
+	$('#signupButton').click(() => {
+		$('#signupSection').slideDown(1500);
+	});
+});
